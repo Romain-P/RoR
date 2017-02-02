46f8db094343a8d471bd45a307be9c7252058635
Romain-P <root@vps370528.ovh.net>
2017-02-02 11:36:56 +0100

    Adding auto www. redirecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_www
+
+  private
+  def redirect_www
+    unless /^www/.match(request.host)
+      redirect_to("#{request.url}".gsub("#{request.protocol}", "#{request.protocol}www."), status: 301)
+    end
+  end
+
   protect_from_forgery with: :exception
 end
